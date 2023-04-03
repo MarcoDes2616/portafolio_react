34201433584a246e3601a884bea9b5ec974a44fa
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-04-03 10:38:33 -0500

    Update Contacto.jsx

--- src/components/contacto/Contacto.jsx
+++ src/components/contacto/Contacto.jsx
@@ -1,19 +1,29 @@
 import "./contacto.css"
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
+import { useState } from "react";
 
 const Contacto = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+    const [active, setActive] = useState(false)
+    const inputNull = { name: "", phone: "", email: "", company: "" };
 
     const sendEmail = data => {
+        console.log("voy a setear");
+        setActive(true)
         axios.defaults.headers.post['Content-Type'] = 'application/json';
         axios.post('https://formsubmit.co/ajax/marco2616@gmail.com', {
             name: `Nuevo mensaje de ${data.name}`,
             message: `${data.name} desea ponerse en contacto contigo, lo puedes contactar este correo ${data.email} o a su numero telefónico ${data.phone}."}`
         })
-            .then(res => console.log(res))
+            .then(res => {
+                reset(inputNull)
+                setActive(false)
+            })
             .catch(error => console.error(error))
     }
+
+    console.log(active);
     return (
         <div className="contacto">
             <h2>Contacto</h2>
@@ -21,24 +31,24 @@ const Contacto = () => {
                 <p>Estaré a gusto de responder a tu contacto a la brevedad posible!</p>
                 <form onSubmit={handleSubmit(sendEmail)}
                 className='form_container'>
-                    <i className='bx bx-mail-send bx-lg'></i>
+                    <i className={ active ? 'bx bx-mail-send bx-fade-right bx-lg' : 'bx bx-mail-send bx-lg'}></i>
                     <div className="names_container">
                         <div className="input_container">
                             <label htmlFor="name">Nombre y Apellido</label>
-                            <input type="text" id='name' placeholder="Nombre"  {...register('name')} />
+                            <input type="text" id='name' placeholder="Nombre y Apellido"  {...register('name')} />
                         </div>
                     </div>
                     <div className="input_container">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id='email'{...register('email')} />
+                        <input type="email" id='email' placeholder="E-mail"{...register('email')} />
                     </div>
                     <div className="input_container">
                         <label htmlFor="phone">Teléfono</label>
-                        <input type="text" id='phone' {...register('phone')} />
+                        <input type="text" id='phone' placeholder="Teléfono" {...register('phone')} />
                     </div>
                     <div className="input_container">
                         <label htmlFor="company">Empresa o Institución que represneta</label>
-                        <input type="text" id='company' {...register('company')} />
+                        <input type="text" id='company' placeholder="Empresa o compañia" {...register('company')} />
                     </div>
                     <button className='submit_btn'>
                         Send
