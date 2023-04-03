29897783d3f30982245143e512aac09aeb2632bf
Marco Cardenas <112307097+MarcoDes2616@users.noreply.github.com>
2023-04-03 01:54:23 -0500

    portafolio

--- src/components/portafolio/Portafolio.jsx
+++ src/components/portafolio/Portafolio.jsx
@@ -1,25 +1,27 @@
 import "./portafolio.css"
 import data from "../../portafolio.json"
-import dl from "../../../public/images/dlirios.png"
 
 const Portafolio = () => {
     return (
         <div className="portafolio_container">
-            <h2 className="portafolio">Portafolio</h2>
+            <h2>Portafolio</h2>
             <div className="cards_container">
-                {/* <div className="card btn">
-                    <div className="face front">
-                        <img src={dl} alt="" />
-                        <p>Titulo</p>
-                    </div>
-                    <div className="face back">
-                        <p>Titulo</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint eligendi, velit natus architecto accusamus quis dolor praesentium sunt ducimus iusto harum repellat tempora assumenda laudantium, ipsa temporibus quisquam repellendus fugit!</p>
-                        <div className="link btn">
-                            ir al sitio
+                {
+                    data.map((page) => (
+                        <div className="card btn">
+                            <div className="face front">
+                                <img src={page.imagen} alt="" />
+                                <p>{page.titulo}</p>
+                            </div>
+                            <div className="face back">
+                                <p>{page.titulo}</p>
+                                <p>{page.descripcion}</p>
+                                <a href={page.url} className="link btn">
+                                    ir al sitio
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                </div> */}
+                    ))}
             </div>
         </div>
     );
